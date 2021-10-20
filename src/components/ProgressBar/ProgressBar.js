c9d6cfaa127e9f7cf939e49179093f203336fc25
Alexandre Gros <xelaflash@gmail.com>
2021-10-20 15:17:19 -0400

    progress bar component ==> size done, border-radius to do

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -4,9 +4,45 @@ import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
+import Filler from './Filler';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const SIZES = {
+    small: {
+      "--height": 8 + "px",
+      "--padding": 0,
+    },
+    medium: {
+      "--height": 12 + "px",
+      "--padding": 0,
+    },
+    large: {
+      "--height": 24 + "px",
+      "--padding": 4 + "px",
+    },
+  };
+  const styles = SIZES[size];
+
+  const ProgressBarStyles = styled.div`
+    height: var(--height);
+    width: 370px;
+    border-radius: 8px;
+    box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+    background-color: ${COLORS.transparentGray15};
+    padding: var(--padding);
+  `;
+
+  return (
+    <ProgressBarStyles
+      style={styles}
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+    >
+      <Filler value={value} />
+    </ProgressBarStyles>
+  );
 };
 
 export default ProgressBar;

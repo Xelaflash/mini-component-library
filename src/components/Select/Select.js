a82f63c3c47eeacd2e1af2abc06e27549a61f2c2
Alexandre Gros <xelaflash@gmail.com>
2021-10-21 11:24:38 -0400

    select to finish after correction

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -6,12 +6,33 @@ import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
 const Select = ({ label, value, onChange, children }) => {
+  const SelectStyles = styled.select`
+    color: ${COLORS.gray700};
+    font-size: 16px;
+    width: fit-content;
+    padding: 12px;
+    border: none;
+    background-color: ${COLORS.transparentGray15};
+    border-radius: 8px;
+    /* appearance: none; */
+    &:hover {
+      color: ${COLORS.black};
+    }
+    label {
+      border:1px solid red;
+    }
+  `;
+
+
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <>
+      <SelectStyles value={value} onChange={onChange}>
+        {/* <Icon id="chevron-down" /> */}
+        {children}
+      </SelectStyles>
+    </>
   );
 };
 

c8b9261ee71f4e50f4d05f0b964243d13c683b9a
Alexandre Gros <xelaflash@gmail.com>
2021-10-22 12:22:15 -0400

    modification after correction

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -4,44 +4,68 @@ import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
-import Filler from './Filler';
 
 const ProgressBar = ({ value, size }) => {
   const SIZES = {
     small: {
       "--height": 8 + "px",
       "--padding": 0,
+      "--radius": 4 + "px",
     },
     medium: {
       "--height": 12 + "px",
       "--padding": 0,
+      "--radius": 4 + "px",
     },
     large: {
-      "--height": 24 + "px",
+      "--height": 16 + "px",
       "--padding": 4 + "px",
+      "--radius": 8 + "px",
     },
   };
-  const styles = SIZES[size];
 
   const ProgressBarStyles = styled.div`
-    height: var(--height);
-    width: 370px;
-    border-radius: 8px;
+    padding: var(--padding);
+    border-radius: var(--radius);
     box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
     background-color: ${COLORS.transparentGray15};
-    padding: var(--padding);
+    /* trim off corners when progress bar is near full */
+    overflow: hidden;
+    `;
+
+  const FillerWrapper = styled.div`
+    border-radius: 4px;
+    /* Trim off corners when progress bar is near-full. */
+    overflow: hidden;
   `;
 
+  const FillerStyles = styled.div`
+    background: ${COLORS.primary};
+    width: var(--width);
+    height: var(--height);
+    border-radius: 4px 0px 0px 4px;
+  `;
+
+    const styles = SIZES[size];
+    if (!styles) {
+      throw new Error(`Unknown size passed to ProgressBar: ${size}`);
+    }
+
   return (
-    <ProgressBarStyles
-      style={styles}
-      role="progressbar"
-      aria-valuenow={value}
-      aria-valuemin="0"
-      aria-valuemax="100"
-    >
-      <Filler value={value} />
-    </ProgressBarStyles>
+    <>
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <ProgressBarStyles
+        style={styles}
+        role="progressbar"
+        aria-valuenow={value}
+        aria-valuemin="0"
+        aria-valuemax="100"
+      >
+        <FillerWrapper>
+          <FillerStyles style={{ width: `${value}%` }} />
+        </FillerWrapper>
+      </ProgressBarStyles>
+    </>
   );
 };
 
